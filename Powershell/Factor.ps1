460ad252c2cc206460df1c1835df0b4189cad6ff
Bad-Relay <36608560+Bad-Relay@users.noreply.github.com>
2021-04-05 13:21:48 -0700

    Add files via upload

---
+++ Powershell/Factor.ps1
@@ -0,0 +1,17 @@
+﻿
+#This script will see if the base variable is a factor of the factor variable 
+
+[int] $base = 10
+[int] $factor = 2
+
+
+
+if (($base % $factor) -eq 0){
+
+Write-Host "True"
+
+}
+else{
+
+Write-Host "Falsle"
+}
---
+++ Powershell/GrowthPopulation.ps1
@@ -0,0 +1,10 @@
+﻿#Used for the Growth of a Population code wars challange 
+
+[int]$p0 = 1500 #stating population 
+[double]$percent = 5 # what percent it is growing 
+[int]$aug = 100 # number of people moving in 
+[int]$p = 5000 # The target population 
+for ($i = 0 ; $p0 -lt $p ; $i++){
+    $p0= $p0 + ($p0 * ( $percent * .01 )+$aug)
+}
+"It will take $i years to get to $p" | Write-Host 
