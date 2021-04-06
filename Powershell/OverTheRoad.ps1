03ac87600960361a6994ec9147e3a284b31ddfbc
Bad-Relay <36608560+Bad-Relay@users.noreply.github.com>
2021-04-06 11:13:34 -0700

    Over The Road challenge

---
+++ Powershell/OverTheRoad.ps1
@@ -0,0 +1,23 @@
+﻿
+<#
+This script takes a input of address and returns the other side of the road. 
+
+Left is odd numbers that go up
+Right is even numbers that go down
+
+Example of a road with a length of 3
+
+1|   |6
+3|   |4
+5|   |2
+
+#>
+
+function Over-Road($address, $length) {
+
+return ($length *2) - ($address-1) #gets the starting number of the right side then subtracts it by the left side
+
+}
+
+# Example use:
+# Over-Road -address 1 -length 3
