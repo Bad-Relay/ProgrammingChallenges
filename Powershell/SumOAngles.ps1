25076b7a82f1c8cb1460826ea1f8d003e0d71ff2
Bad-Relay <36608560+Bad-Relay@users.noreply.github.com>
2021-04-07 11:43:40 -0700

    Add files via upload

---
+++ Powershell/SumOAngles.ps1
@@ -0,0 +1,12 @@
+﻿<#
+This script will display the angle of a shape given the number of sides
+#>
+
+
+function Get-Angle([int] $sides) {
+  ($sides - 2) * 180
+}
+
+
+# Example Use
+#Get-Angle -sides 4
